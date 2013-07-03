e55a0ecb02bfe87e6dc281776431e7cba5bad35e
mecwerks <mecwerks@gmail.com>
2013-07-03 16:21:02 -0400

    updated build script

--- build-vms.sh
+++ build-vms.sh
@@ -6,16 +6,19 @@ echo "Building qagame.qvm"
 cd code/game
 ./game.sh
 cp vm/qagame.qvm ../../vm/
+rm -rf vm/
 
 echo "Building cgame.qvm"
 cd ../cgame
 ./cgame.sh
 cp vm/cgame.qvm ../../vm/
+rm -rf vm/
 
 echo "Building ui.qvm"
 cd ../q3_ui
 ./q3_ui.sh
 cp vm/ui.qvm ../../vm/
+rm -rf vm/
 
 echo "Packaging pak9.pk3 with config file"
 cd ../../
